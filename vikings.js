80b80f5cda5697bbea5b0ce95e9b6e3c209098c7
L.Carquet <laetiticarquet@gmail.com>
2016-09-11 09:10:06 +0200

    second iteration of exercise

--- vikings.js
+++ vikings.js
@@ -1,6 +1,6 @@
 var Viking = function(name) {
-    this.name = name;
-    this.health = 4;
+    this.name = name
+    this.health = Math.floor(Math.random()*(12-3)+ 3);
     this.fight = function() {
         var punchstrength = Math.random();
             
@@ -8,39 +8,76 @@ var Viking = function(name) {
         };
 };
 
+var Saxon = function() {
+    this.health = Math.floor(Math.random()*(10-0)+0);
+    this.fight = function() {
+        var punchstrength = Math.random();
+            
+            return Math.floor(punchstrength * 90);
+    };
+};
+
+
 
 var Pit = function(viking1,viking2) {
     this.fight = function(){
-        var fight= 0;
+        var round = 0;
         while ((viking1.health > 1) && (viking2.health > 1)){
             if (viking1.fight() === viking2.fight()) {
                     viking1.health = viking1.health;
                     viking2.health = viking2.health;
-                console.log("Its a tie! Nobody loses life!");
+               
                 console.log(viking1.name + " remaining life is: "+ viking1.health);
                 console.log(viking2.name + " remaining life is: "+ viking1.health);
             } else if (viking1.fight() > viking2.fight()) {
-                console.log(viking1.name + " wins :");
+                console.log(viking1.name + " wins : ");
                 console.log(viking2.name + " remaining lives :", viking2.health -= 1);
             } else {
                 console.log(viking2.name + " wins :");
                 console.log(viking1.name +"remaining lives :", viking1.health -= 1);
             }
-            fight++;
+            round++;
         }; 
     };
 };
 
-  
+
+
+
+var Village = function(viking, saxon) {
+    this.fight = function(){
+        var round = 0;
+        while ((viking.health > 0) && (saxon.health > 0)){
+            if (viking.fight() === saxon.fight()) {
+                    viking.health = viking.health;
+                    saxon.health = saxon.health;
+               // console.log("Its a tie! Nobody loses life!");
+                console.log(viking.name + " remaining lives is: "+ viking.health);
+                console.log("Saxon remaining lives is: "+ saxon.health);
+            } else if (viking.fight() > saxon.fight()) {
+                console.log(viking.name + " wins!");
+                console.log("Saxon remaining lives : ", saxon.health -= 1);
+            } else {
+                console.log("Saxon wins !");
+                console.log(viking.name + "remaining lives :", viking.health -= 1);
+            }
+            round ++;
+        }; 
+    };
+};
 
 
 var myViking = new Viking("Urkul");
-var myOtherviking = new Viking("Zunkar");
-pit1 = new Pit(myViking,myOtherviking);
-pit1.fight();
-// console.log(myViking.fight())
-// console.log ("here are the two constestants: " + myViking.name + " " + myOtherviking.name);
+//var myOtherviking = new Viking("Zunkar");
+//var myThirdViking = new Viking("Blurb");
+var saxon1 = new Saxon();
 
-// console.log ("Your viking: " + myViking.name + " gave a punch, his strengh is: " + myViking.fight());
+var village1 = new Village(myViking,saxon1);
+village1.fight();
 
-// console.log ("Your viking: " + myOtherviking.name + " gave a punch, his strengh is: " + myOtherviking.fight());
+
+//var vikingArmy = [myViking, myOtherviking, myThirdViking];
+
+
+//pit1 = new Pit(myViking,myOtherviking);
+//pit1.fight();
